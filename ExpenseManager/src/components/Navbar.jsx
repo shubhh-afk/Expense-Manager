d70e0c1705974ff85b28891828ada0e22ceffe7a
Shubh <sahilraj2310@gmail.com>
2025-03-06 16:39:28 +0530

    complete navbar

--- ExpenseManager/src/components/Navbar.jsx
+++ ExpenseManager/src/components/Navbar.jsx
@@ -1,13 +1,42 @@
 import { Link } from "react-router-dom";
+import Input from "./Input";
+import Button from "./Button";
+import { useState } from "react";
+import InputButton from "./InputButton";
 
 export default function Navbar() {
+  const [trackingNumber, setTrackingNumber] = useState("");
+
   return (
-    <nav className="w-full flex justify-between items-center p-4">
+    <nav className="w-full mt-0 h-15 flex bg-gray-400 justify-between items-center px-5">
       <h1 className="text-xl font-bold">Expense Manager</h1>
-      <div className="space-x-4">
-        <Link to="/dashboard" className="hover:underline">Dashboard</Link>
-        <Link to="/create-expense" className="hover:underline">Create Expense</Link>
-        <Link to="/manage-expenses" className="hover:underline">Manage Expenses</Link>
+      <div className=" flex gap-1.3">
+        <Input
+          type="text"
+          placeholder="ENTER SERIES"
+          className="text-sm font-light flex items-center p-1 w-32"
+        />
+        <Input
+          type="text"
+          placeholder="PINCODE"
+          className="text-sm font-light flex items-center p-1 w-32"
+        />
+        <Input
+          type="text"
+          placeholder="PLEASE ENTER CENTER/HUB"
+          className="text-sm font-light flex w-auto items-center p-1"
+        />
+        <div className="flex justify-center mt-2 items-center">
+          <InputButton
+            type="text"
+            placeholder="Shipment/Bag Number"
+            className="text-md font-light"
+            buttonText="Track"
+            btnClassName=" bg-orange-500 
+            hover:bg-orange-600 border-1 border-t border-r border-b
+"
+          />
+        </div>
       </div>
     </nav>
   );
