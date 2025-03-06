e8e8196a560871e55499aeba72bb1e98f81d5e7f
Shubh <sahilraj2310@gmail.com>
2025-03-06 16:15:29 +0530

    minor changes

--- ExpenseManager/src/components/Input.jsx
+++ ExpenseManager/src/components/Input.jsx
@@ -7,19 +7,20 @@ function Input({
   value,
   onChange,
   className = "",
+  containerClassName = "",
   error,
   ...props
 }) {
   return (
-    <div className="flex gap-2 mt-2 p-2 items-center">
-      {label && <label className="block text-sm mb-1">{label}:</label>}
+    <div className={`flex gap-2 mt-2 p-2 items-center ${containerClassName}`}>
+      {label && <label className="block text-sm mb-1">{label}</label>}
       <span>
         <input
           type={type}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
-          className={`border bg-[#e9ecef] border-orange-600 px-1.5 py-0.5 focus:outline-none focus:ring-1 focus:ring-orange-500 ${className}`}
+          className={`px-2 py-0.5 rounded-xs border bg-gray-50 border-orange-500  focus:outline-none focus:ring-1 focus:ring-orange-600 ${className}`}
           {...props}
         />
       </span>
