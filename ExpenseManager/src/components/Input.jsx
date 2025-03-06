9a7590836e4d6b77342a2b9c6d664b2a903de7ca
Shubh <sahilraj2310@gmail.com>
2025-03-06 11:15:13 +0530

    Input field component added

---
+++ ExpenseManager/src/components/Input.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Input({
+    label,
+  type = "text",
+  placeholder = "",
+  value,
+  onChange,
+  className = "",
+  error,
+  ...props
+}) {
+  return (
+    <div className="flex gap-2 mt-2 p-2 items-center">
+    {label && <label className="block text-sm mb-1">{label}:</label>}
+    <span>
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="border border-orange-600  focus:outline-none focus:ring-1 focus:ring-orange-500"
+    />
+    </span>
+    {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
+  </div>
+  )
+}
+
+export default Input
