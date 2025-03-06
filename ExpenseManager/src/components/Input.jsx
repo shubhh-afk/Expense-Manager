c9c369d9b7f3e7217f942e1744ace6e8124ba065
Shubh <sahilraj2310@gmail.com>
2025-03-06 11:33:06 +0530

    minor changes

--- ExpenseManager/src/components/Input.jsx
+++ ExpenseManager/src/components/Input.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Input({
-    label,
+  label,
   type = "text",
   placeholder = "",
   value,
@@ -12,19 +12,20 @@ function Input({
 }) {
   return (
     <div className="flex gap-2 mt-2 p-2 items-center">
-    {label && <label className="block text-sm mb-1">{label}:</label>}
-    <span>
-    <input
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className="border border-orange-600  focus:outline-none focus:ring-1 focus:ring-orange-500"
-    />
-    </span>
-    {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
-  </div>
-  )
+      {label && <label className="block text-sm mb-1">{label}:</label>}
+      <span>
+        <input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={`border border-orange-600 px-1 py-0.5 focus:outline-none focus:ring-1 focus:ring-orange-500 ${className}`}
+          {...props}
+        />
+      </span>
+      {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
+    </div>
+  );
 }
 
-export default Input
+export default Input;
