7f84296e9a93fcfa6bd28135881566d4c47b2e63
Shubh <sahilraj2310@gmail.com>
2025-03-07 14:46:00 +0530

    restructured

--- ExpenseManager/src/components/Layout.jsx
+++ ExpenseManager/src/components/Layout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "./Navbar";
+import SideBar from "./SideBar";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-[#e9ecef]">
-      <Navbar />
-      <div className="flex justify-center items-center h-full w-full">
-        <Outlet />
+    <div className="flex min-h-screen bg-[#e9ecef]">
+      <SideBar />
+      <div className="flex flex-grow flex-col">
+        <Navbar />
+        <div className="flex justify-center items-center h-full w-full">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
