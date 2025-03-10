9bed515066921d8eb0be0d7384508e16f5ae6f46
Shubh <sahilraj2310@gmail.com>
2025-03-11 02:46:45 +0530

    final button component

--- ExpenseManager/src/components/Button.jsx
+++ ExpenseManager/src/components/Button.jsx
@@ -4,13 +4,13 @@ function Button({
   buttonText,
   type = "button",
   bgColor = "",
-  textColor = "text-white",
+  textColor = "text-gray-900",
   className = "",
   ...props
 }) {
   return (
     <button
-      className={`text-center px-4 py-2 rounded-md ${bgColor} ${textColor} ${className}`}
+      className={`text-center bg-gradient-to-tr from-orange-400 to-orange-300 h-10 hover:bg-gradient-to-tr hover:from-orange-400 hover:to-orange-600 font-semibold px-4 py-2 rounded-md cursor-pointer ${bgColor} ${textColor} ${className}`}
       {...props}
     >
       {buttonText}
