777efd54e4ac1e0c69049bdb25cd4aec1065a2a1
Shubh <sahilraj2310@gmail.com>
2025-03-11 03:28:20 +0530

    final footer

--- ExpenseManager/src/components/Footer.jsx
+++ ExpenseManager/src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="w-full text-center border-t border-orange-500 bg-gray-900 text-orange-400 py-4">
+    <footer className="w-full flex justify-center gap-4 text-center border-t border-orange-500 bg-gray-900 text-orange-400 px-3 py-2">
       <span>Copyright © 2024-2025 </span>
       <a
         href="https://www.shreemaruti.com"
         className="text-orange-500 font-bold hover:underline hover:text-orange-300"
         target="_blank"
-        rel="noopener noreferrer"
+        rel=""
       >
         SHREE MARUTI INTEGRATED LOGISTICS LIMITED
       </a>
