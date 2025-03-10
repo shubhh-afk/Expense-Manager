b183221e0f0d8c14907146762da8308b4247f341
Shubh <sahilraj2310@gmail.com>
2025-03-11 03:28:58 +0530

    layout created

--- ExpenseManager/src/components/Layout.jsx
+++ ExpenseManager/src/components/Layout.jsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import SideBar from "./SideBar";
+import SideBar, { SideBarItems } from "./SideBar";
+import Footer from "./Footer";
+import { CircleUserRound, Home, LayoutDashboard, Receipt } from "lucide-react";
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen bg-[#e9ecef]">
-      <SideBar />
+    <div className="flex min-h-screen bg-gray-900">
+      <SideBar>
+        <SideBarItems icon={<CircleUserRound size={30} />} text="Profile" />
+        <SideBarItems icon={<LayoutDashboard size={30} />} text="Dashboard" />
+        <SideBarItems icon={<Receipt size={30} />} text="Expenses" active />
+      </SideBar>
       <div className="flex flex-grow flex-col">
         <Navbar />
         <div className="flex justify-center items-center h-full w-full">
           <Outlet />
+        </div>
+        <div className="w-full h-16 flex justify-center">
+          <Footer />
         </div>
       </div>
     </div>
